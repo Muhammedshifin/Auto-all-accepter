f625512a8aaac82e0140c5c10de50bb736ca1fbc
Muhammedshifin <muhammedshifink4@gmail.com>
2023-02-02 19:42:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,10 @@ Bot = Client(name="auto-delete",
              workers=300
              )
 
+START_MSG = "<b>Hai {},\nI'm a private bot of MaSTeR to Accept All users in one time</b>"
+CHAT = "#chatid*
+
+
 @Bot.on_message(filters.command('start') & filters.private)
 async def def start(bot, message):
     await message.reply(START_MSG.format(message.from_user.mention))
@@ -28,4 +32,21 @@ async def accept(bot, message):
     lol = await message.reply_text(
         text ='Proccesing'
     )
+    await User.approve_all_chat_join_requests(CHAT)
+    await lol.edit(f"Completed")
+    print("Completed")
     
+ 
+
+User.start()
+print("User Started!")
+Bot.start()
+print("Bot Started!")
+
+idle()
+
+User.stop()
+print("User Stopped!")
+Bot.stop()
+print("Bot Stopped!")
+
