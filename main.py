34879e32a4cd8ce7a54cd1eebd468ef2515fbcf6
Muhammedshifin <muhammedshifink4@gmail.com>
2023-02-02 19:25:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import asyncio
+from os import environ
+from pyrogram import Client, filters, idle
 
 
-User
-ser = Client(name="user-account",
+
+User = Client(name="user-account",
               session_string=SESSION,
               api_id=API_ID,
               api_hash=API_HASH,
@@ -15,3 +18,14 @@ Bot = Client(name="auto-delete",
              bot_token=BOT_TOKEN,
              workers=300
              )
+
+@Bot.on_message(filters.command('start') & filters.private)
+async def def start(bot, message):
+    await message.reply(START_MSG.format(message.from_user.mention))
+    
+@Bot.on_message(filters.command("run") & filters.user(ADMINS))
+async def accept(bot, message):
+    lol = await message.reply_text(
+        text ='Proccesing'
+    )
+    
